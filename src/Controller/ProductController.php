0f092773b3141edd8aeecbfa680aed7e4509f20f
SYLVIE Linxe <sylvie.linxe@live.fr>
2022-05-05 17:44:03 +0200

    creation page détail produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,7 @@ class ProductController extends AbstractController
     //Je me fais livrer avec le repository ma table catégorie de ma bdd
     public function category($slug, CategoryRepository $categoryRepository): Response
     {
-        // Je demande a afficher 1 catégoriegrâce au critère findoneby
+        // Je demande à afficher 1 catégorie grâce au critère findoneby
         $category = $categoryRepository->findOneBy([
             // Le critère : recevoir le slug reçu dans l'URL
             'slug' => $slug
@@ -30,6 +31,26 @@ class ProductController extends AbstractController
             'slug' => $slug,
             // Je passe la catégory au template twig
             'category' => $category
+        ]);
+    }
+
+
+    // C'est l'URL qu' permettre d'acceder au produit
+    #[Route('/{category_slug}/{slug}', name:'product_show')]
+    //Je créée la function qui permettra d'afficher un seul produit en recevant mon slug et en permettant aux données de ma bdd d'etre remonté de la table des produits grace à productRepository
+    public function show($slug, ProductRepository $productRepository) {
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        //Je lance une nouvelle exeption si le produit n'existe pas
+        if(!$product)
+        {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
