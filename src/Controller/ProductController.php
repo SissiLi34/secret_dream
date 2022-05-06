e3358e14884dc9a87901a0857b5b7ad7cea0a45c
SYLVIE Linxe <sylvie.linxe@live.fr>
2022-05-06 18:02:05 +0200

    mise en place formulaire admin
    -Champs du formulaire
    -Menu déroulant

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -53,6 +58,73 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    #[Route('/admin/product/create', name:'product_create')]
+    //création du formulaire création de produit dans laquelle je me fais livrer le CategoryRepository pour avoir accès a la bdd
+    public function create(FormFactoryInterface $factory, CategoryRepository $categoryRepository) 
+    {
+        $builder = $factory->createBuilder();
+        //Je construis mo formulaire
+        //J'importe la class que je souhaite
+        $builder->add('name', TextType::class, [
+            //je passe tous mes paramètre dans le tableau
+            'label' => 'Nom du produit',
+            'attr' => [
+                //Form bootstrap
+                'class' => 'form-control', 
+                'placeholder' => 'Tapez le nom du produit']
+        ])
+            //Descrition avec textarea
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur'
+                ]
+            ])
+            //Ajout du prix
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du produit',
+                'attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'Tapez le prix du produit en €'
+                ]
+                ]);
+
+//Je stoppe mes add pour rajouter un tableau option pour le menu déroulant
+
+//Les optins sont égale à un tableau
+$options = [];
+//Pour chaque catégorie que je trouve dans la bdd et que je nome $category
+foreach($categoryRepository->findAll() as $category) {
+    //Je créé dans mes options une clés qui soit le nom de la catégorie et la valeur qui sera l'identifiant de la catégorie
+    $options[$category->getName()] = $category->getId();
+    
+}
+
+//dd($options);
+
+
+                
+            //Menu déroulant
+            $builder->add('category', ChoiceType::class, [
+                'label' => 'Catégorie',
+                'attr' => [
+                'class' => 'form-control'],
+                'placeholder' => '--Choisir une catégorie--',
+                //Je passe dans mes choix le tableau option construit au dessus
+                'choices' => $options
+            ]);
+
+            $form = $builder->getForm();
+            //Cette class va me permettre d'afficher la vue de mon formulaire
+            $formView = $form->createView();
+        
+        return $this->render('product/create.html.twig', [
+            //Je passe la variable formView (repésenté par la variable php formView) à twig pour qu'il l'affiche
+            'formView' => $formView
         ]);
     }
 }
